1865d52a395e332933e761ccce2471eb2b613730
letnagao <leticia-nagao@hotmail.com>
2022-07-07 00:07:58 -0300

    added programs component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import HeroSection from './components/hero-section/HeroSection';
+import Programs from './components/programs/Programs';
 
 function App() {
   return (
     <div className="App">
-      <HeroSection />
+      <HeroSection/>
+      <Programs/>
     </div>
   );
 }
---
+++ src/components/programs/Programs.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {programsData} from '../../data/programsData';
+import RightArrow from '../../assets/rightArrow.png';
+
+import './Programs.css';
+
+const Programs = () => {
+  return (
+    <div className="Programs" id="programs">
+        <div className="programs-header">
+            <span className="stroke-text">Explore our</span>
+            <span>Programs</span>
+            <span className="stroke-text">to shape you</span>
+        </div>
+        <div className="program-categories">
+            {programsData.map((program) => (
+                <div className="category">
+                    {program.image}
+                    <span>{program.heading}</span>
+                    <span>{program.details}</span>
+                    <div className="join-now"><span>Join Now</span><img src={RightArrow}/></div>
+                </div>
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default Programs;
